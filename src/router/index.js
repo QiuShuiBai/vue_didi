f5b6f96442a40cee395d06c509d1f98d7249faf7
QiuShuiBai <1125097005@qq.com>
2018-01-24 02:40:38 +0800

    配置导航栏相关路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,10 @@
 import Vue from "vue"
 import Router from "vue-router"
-import HelloWorld from "@/components/HelloWorld"
+import fastCar from "@/components/routerComponents/fastCar.vue"
+import taxiCar from "@/components/routerComponents/taxiCar.vue"
+import freeCar from "@/components/routerComponents/freeCar.vue"
+import desDri from "@/components/routerComponents/desDri.vue"
+import borCar from "@/components/routerComponents/borCar.vue"
 
 Vue.use(Router)
 
@@ -8,8 +12,33 @@ export default new Router({
   routes: [
     {
       path: "/",
-      name: "HelloWorld",
-      component: HelloWorld
+      name: "fastCar",
+      redirect: "./fastCar"
+    },
+    {
+      path: "/fastCar",
+      name: "fastCar",
+      component: fastCar
+    },
+    {
+      path: "/taxiCar",
+      name: "taxiCar",
+      component: taxiCar
+    },
+    {
+      path: "/freeCar",
+      name: "freeCar",
+      component: freeCar
+    },
+    {
+      path: "/desDri",
+      name: "desDri",
+      component: desDri
+    },
+    {
+      path: "/borCar",
+      name: "borCar",
+      component: borCar
     }
   ]
 })
